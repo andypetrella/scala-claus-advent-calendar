bf8173d6b50758913f0820414c47f2348e3f61e7
noootsab <andy.petrella@gmail.com>
2014-01-09 01:11:39 +0100

    Switch to custom play-2.3-snapshot version to handle compilation failure by updating the action which no more compile with the very first version -- which fails but has the right range of lines for further saving

--- app/controllers/DayTmpl.scala
+++ app/controllers/DayTmpl.scala
@@ -1,17 +1,17 @@
 package controllers
 
-import play.api.mvc.{SimpleResult, Request, Action}
+import play.api.mvc._
 import scala.concurrent.Future
 import play.api.mvc.Results._
-import play.api.mvc.SimpleResult
-import play.templates.TemplateMagic.RichDate
-import play.api.templates.{Html, HtmlFormat}
-import play.templates.Format
-import scala.reflect.ClassTag
+import play.api.templates.HtmlFormat
 import scalax.file.Path
 
+import play.api.mvc.SimpleResult
+
 trait DayTmpl[A, M] extends Action[A] {
-  val file:Path = Path(".") / "app" / "controllers" / (this.getClass.getSimpleName+".scala")
+  import DayTmpl._
+  val day:Int = this.getClass.getSimpleName.drop("Day".length).toInt
+  val file:Path = fileFor(day)//Path(".") / "app" / "controllers" / (this.getClass.getSimpleName+".scala")
 
   implicit val code = file.lines()
                           .zipWithIndex
@@ -28,5 +28,35 @@ trait DayTmpl[A, M] extends Action[A] {
   }
 
   def sync:M
-
+}
+object DayTmpl {
+  def fileFor(day:Int):Path = Path(".") / "app" / "controllers" / s"Day$day.scala"
+  def actionFor[A](day:Int, parser:BodyParser[A]):DayTmpl[A, _] =
+    day match {
+      case 1 => Day1(parser)
+      case 2 => Day2(parser)
+      case 3 => Day3(parser)
+      case 4 => Day4(parser)
+      case 5 => Day5(parser)
+      case 6 => Day6(parser)
+      case 7 => Day7(parser)
+      case 8 => Day8(parser)
+      case 9  => Day9(parser)
+      case 10 => Day10(parser)
+      case 11 => Day11(parser)
+      case 12 => Day12(parser)
+      case 13 => Day13(parser)
+      case 14 => Day14(parser)
+      //case 15 => Day15(parser)
+      //case 16 => Day16(parser)
+      //case 17 => Day17(parser)
+      //case 18 => Day18(parser)
+      //case 19 => Day19(parser)
+      //case 20 => Day20(parser)
+      //case 21 => Day21(parser)
+      //case 22 => Day22(parser)
+      //case 23 => Day23(parser)
+      //case 24 => Day24(parser)
+      //case 25 => Day25(parser)
+    }
 }
--- app/controllers/TalkAction.scala
+++ app/controllers/TalkAction.scala
@@ -7,6 +7,7 @@ import play.api.mvc.Results._
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.mvc
+import scalax.file.Path
 
 object TalkAction extends ActionBuilder[Request] {
   protected def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[mvc.SimpleResult]): Future[mvc.SimpleResult] =
@@ -20,44 +21,22 @@ case class DayAction[A](action: Action[A]) extends Action[A] {
     single(("day",number))
   )
 
+  implicit class WinPath(p:Path) {
+    val updateForWin = p.toAbsolute.path.replaceAll("\\\\", "\\\\\\\\")
+  }
+
   def apply(request: Request[A]): Future[SimpleResult] = {
     implicit val r = request
 
     day.bindFromRequest.fold(
       hasErrors = _ => Future.successful(BadRequest("Unknown day!")),
       success = day => {
-        val tmpl: DayTmpl[A, _] = day match {
-          case 1 => Day1(parser)
-          case 2 => Day2(parser)
-          case 3 => Day3(parser)
-          case 4 => Day4(parser)
-          case 5 => Day5(parser)
-          case 6 => Day6(parser)
-          case 7 => Day7(parser)
-          case 8 => Day8(parser)
-          case 9  => Day9(parser)
-          case 10 => Day10(parser)
-          case 11 => Day11(parser)
-          case 12 => Day12(parser)
-          case 13 => Day13(parser)
-          case 14 => Day14(parser)
-          //case 15 => Day15(parser)
-          //case 16 => Day16(parser)
-          //case 17 => Day17(parser)
-          //case 18 => Day18(parser)
-          //case 19 => Day19(parser)
-          //case 20 => Day20(parser)
-          //case 21 => Day21(parser)
-          //case 22 => Day22(parser)
-          //case 23 => Day23(parser)
-          //case 24 => Day24(parser)
-          //case 25 => Day25(parser)
-        }
+        val tmpl: DayTmpl[A, _] = DayTmpl.actionFor[A](day, parser)
         try {
           tmpl(request)
         } catch {
           case e:Throwable => {
-            Future.successful(NotImplemented(views.html.todo(day, tmpl.file.toAbsolute.path.replaceAll("\\\\", "\\\\\\\\"), tmpl.code) ))
+            Future.successful(NotImplemented(views.html.todo(day, tmpl.file.updateForWin, tmpl.code) ))
           }
         }
       }
