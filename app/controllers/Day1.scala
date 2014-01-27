a0599267efc7207b11235b9191903aff70a02b86
Andy Petrella <andy.petrella@gmail.com>
2014-01-27 12:11:12 +0100

    Update Day1.scala

--- app/controllers/Day1.scala
+++ app/controllers/Day1.scala
@@ -9,17 +9,16 @@ case class Day1[A](parser:BodyParser[A]) extends DayTmpl[A, String] {
 
   def sync:String = { // this is a method|function
     // Scala is JVM based
-    var date:java.util.Date = ???;
+    var date:java.util.Date = ????("null");
     // and can use whatever Java libs
     var log:org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger("Day1");
-    var jodaTime:org.joda.time.DateTime = ???;
-    jodaTime = new org.joda.time.DateTime()
-      .withDayOfMonth(1)
-      .withMonthOfYear(org.joda.time.DateTimeConstants.DECEMBER);
+    var jodaTime:org.joda.time.DateTime = ????("new org.joda.time.DateTime()");
+    jodaTime =  jodaTime.withDayOfMonth(1)
+                        .withMonthOfYear(org.joda.time.DateTimeConstants.DECEMBER);
 
     log.trace(jodaTime.toString());
 
-    date = ???;
+    date = ????("jodaTime.toDate()");
 
     return org.apache.commons.lang3.time.DateFormatUtils.ISO_DATE_FORMAT.format(date);
   }
