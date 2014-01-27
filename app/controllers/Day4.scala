78c0ddedfc2ac36f7da8d5e85040b95ba82aee63
Andy Petrella <andy.petrella@gmail.com>
2014-01-27 15:21:06 +0100

    Update Day4.scala

--- app/controllers/Day4.scala
+++ app/controllers/Day4.scala
@@ -23,10 +23,10 @@ case class Day4[A](parser:BodyParser[A]) extends DayTmpl[A, String] {
     //Create Set using List builder
     var users:Set[User] = Set(List.fill(10) {
       //Create less than 50 random tweets with random status of length <= 140
-      ???
+      ????("List.fill(nextInt(50)) Tweets")
       i+=1
       // Create a User
-      ???
+      ????("new User")
     }:_*)
 
     var info:ListBuffer[String] = new ListBuffer[String]()
